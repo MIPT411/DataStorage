0b37a2ba3ae3a076ad7a263881bc29066b28868b
Egor Sbrodov <egor-ulstu@yandex.ru>
2020-10-01 22:49:05 +0300

    Project structure added

---
+++ src/__main__.py
@@ -0,0 +1,10 @@
+from .app import app
+from .config import WEB_HOST, WEB_PORT, WEB_DEBUG
+# Include modules
+from .modules import auth_module
+
+if __name__ == "__main__":
+    # Register modules
+    app.register_blueprint(auth_module)
+    # Run service
+    app.run(host=WEB_HOST, port=WEB_PORT, debug=WEB_DEBUG)
